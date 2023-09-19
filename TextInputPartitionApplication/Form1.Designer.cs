d1ebdaad4da5e2ea9bdd6a8a3c858f5526cf30d6
JordanLaurent09 <128598320+JordanLaurent09@users.noreply.github.com>
2023-09-19 18:32:07 +0200

    winforms

---
+++ TextInputPartitionApplication/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿
+namespace TextInputPartitionApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CheckBtn = new System.Windows.Forms.Button();
+            this.UserInputTB = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // CheckBtn
+            // 
+            this.CheckBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 27F);
+            this.CheckBtn.Location = new System.Drawing.Point(12, 324);
+            this.CheckBtn.Name = "CheckBtn";
+            this.CheckBtn.Size = new System.Drawing.Size(309, 114);
+            this.CheckBtn.TabIndex = 0;
+            this.CheckBtn.Text = "Записать элементы";
+            this.CheckBtn.UseVisualStyleBackColor = true;
+            this.CheckBtn.Click += new System.EventHandler(this.CheckBtn_Click);
+            // 
+            // UserInputTB
+            // 
+            this.UserInputTB.Location = new System.Drawing.Point(12, 18);
+            this.UserInputTB.Multiline = true;
+            this.UserInputTB.Name = "UserInputTB";
+            this.UserInputTB.Size = new System.Drawing.Size(309, 300);
+            this.UserInputTB.TabIndex = 1;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(366, 18);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 420);
+            this.listBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(498, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.UserInputTB);
+            this.Controls.Add(this.CheckBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CheckBtn;
+        private System.Windows.Forms.TextBox UserInputTB;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ TextInputPartitionApplication/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TextInputPartitionApplication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CheckBtn_Click(object sender, EventArgs e)
+        {
+            ListBoxOperator(StringToArray(UserInputTB.Text));
+        }
+
+        private void ListBoxOperator(string[] arrayOfString)
+        {
+            for(int i = 0; i < arrayOfString.Length; i++)
+            {
+                listBox1.Items.Add(arrayOfString[i]);
+            }
+        }
+
+        private string[] StringToArray(string sourceString)
+        {
+            return sourceString.Split(' ');
+        }
+    }
+}
