c02473b1a210b58422e88d742bd6b5f4e0e8d480
JordanLaurent09 <128598320+JordanLaurent09@users.noreply.github.com>
2023-09-20 15:08:49 +0200

    winforms

---
+++ TreeViewApplication/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿
+namespace TreeViewApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("Яблоки");
+            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("Груши");
+            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("Бананы");
+            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("Фрукты", new System.Windows.Forms.TreeNode[] {
+            treeNode1,
+            treeNode2,
+            treeNode3});
+            System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("Помидоры");
+            System.Windows.Forms.TreeNode treeNode6 = new System.Windows.Forms.TreeNode("Овощи", new System.Windows.Forms.TreeNode[] {
+            treeNode5});
+            System.Windows.Forms.TreeNode treeNode7 = new System.Windows.Forms.TreeNode("Продукты", new System.Windows.Forms.TreeNode[] {
+            treeNode4,
+            treeNode6});
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(100, 44);
+            this.treeView1.Name = "treeView1";
+            treeNode1.Name = "Узел2";
+            treeNode1.Text = "Яблоки";
+            treeNode2.Name = "Узел3";
+            treeNode2.Text = "Груши";
+            treeNode3.Name = "Узел4";
+            treeNode3.Text = "Бананы";
+            treeNode4.Name = "subNode";
+            treeNode4.Text = "Фрукты";
+            treeNode5.Name = "Узел6";
+            treeNode5.Text = "Помидоры";
+            treeNode6.Name = "Узел5";
+            treeNode6.Text = "Овощи";
+            treeNode7.Name = "MainRoot";
+            treeNode7.Text = "Продукты";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode7});
+            this.treeView1.Size = new System.Drawing.Size(197, 97);
+            this.treeView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(97, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(200, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Пример по созданию дерева вручную";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
